d7bea4957c9b59e7ca9c3e68f88881a6eac5916e
Andy Hoffman <antibland@gmail.com>
2015-03-18 02:01:47 -0400

    Move JS to external file

---
+++ game.js
@@ -0,0 +1,165 @@
+"use strict";
+var canvas         = document.createElement("canvas"),
+    button         = document.querySelector("button"),
+    context        = canvas.getContext("2d"),
+    CANVAS_WIDTH   = 512,
+    CANVAS_HEIGHT  = 480,
+    BADGER_WIDTH   = 60,
+    BADGER_HEIGHT  = 43,
+    EXEC_WIDTH     = 32,
+    EXEC_HEIGHT    = 48,
+    keysDown       = [],
+    w              = window,
+    playfieldReady = false,
+    badgerReady    = false,
+    execReady      = false,
+    playfieldImage,
+    badgerImage,
+    execImage,
+    coverImage,
+    reset,
+    update,
+    render,
+    main,
+    then;
+
+// Game objects
+var badger         = {
+      speed: 256
+    },
+    exec        = {},
+    execsCaught = 0;
+
+function init() {
+  then = Date.now();
+  reset();
+  main();
+}
+
+function createPattern(theImg) {
+  var ptrn = context.createPattern(theImg, "repeat");
+  context.fillStyle = ptrn;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function loadImages() {
+  var imgPrefix = ["playfield", "badger", "exec"];
+
+  function loadImage(imgName, index) {
+    var img       = window[imgName + "Image"],
+        imgReady  = window[imgName + "Ready"],
+        imgPrefix = imgName.indexOf("badger") >= 0 ||
+                    imgName === "exec" ? ".png" : ".jpg";
+
+    window[imgName + "Image"] = new Image();
+    window[imgName + "Image"].src = "assets/" + imgName + imgPrefix;
+
+    window[imgName + "Image"].onload = function() {
+      window[imgName + "Ready"] = true;
+    }
+  }
+
+  imgPrefix.forEach(loadImage);
+}
+
+loadImages();
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+document.body.appendChild(canvas);
+
+// Cover background image
+coverImage = new Image();
+coverImage.src = "assets/cover.jpg";
+coverImage.onload = function() {
+  createPattern(coverImage);
+};
+
+addEventListener("keydown", function (e) {
+  keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+  delete keysDown[e.keyCode];
+}, false);
+
+button.addEventListener("click", init, false);
+
+// Reset the game when the player catches a exec
+reset = function () {
+  badger.x = canvas.width / 2;
+  badger.y = canvas.height / 2;
+
+  // Throw the exec somewhere on the screen randomly
+  exec.x = 32 + (Math.random() * (canvas.width - 64));
+  exec.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+update = function (modifier) {
+  if (38 in keysDown && badger.y > 0) { // UP
+    badger.y -= badger.speed * modifier;
+  }
+  if (40 in keysDown && badger.y < CANVAS_HEIGHT - BADGER_HEIGHT) { // DOWN
+    badger.y += badger.speed * modifier;
+  }
+  if (37 in keysDown && badger.x > 0) { // LEFT
+    badger.x -= badger.speed * modifier;
+  }
+  if (39 in keysDown && badger.x < CANVAS_WIDTH - BADGER_WIDTH) { // RIGHT
+    badger.x += badger.speed * modifier;
+  }
+
+  // Collision check
+  if (
+    badger.x <= (exec.x + EXEC_WIDTH)
+    && exec.x <= (badger.x + BADGER_WIDTH)
+    && badger.y <= (exec.y + EXEC_HEIGHT)
+    && exec.y <= (badger.y + BADGER_HEIGHT)
+  ) {
+    ++execsCaught;
+    reset();
+  }
+};
+
+// Draw everything
+render = function () {
+  if (playfieldReady) {
+    createPattern(playfieldImage);
+  }
+
+  if (badgerReady) {
+    context.drawImage(badgerImage, badger.x, badger.y);
+  }
+
+  if (execReady) {
+    context.drawImage(execImage, exec.x, exec.y);
+  }
+
+  // Score
+  context.fillStyle = "rgb(250, 250, 250)";
+  context.font = "24px Helvetica";
+  context.textAlign = "left";
+  context.textBaseline = "top";
+  context.fillText("" + execsCaught, 32, 32);
+};
+
+// The main game loop
+main = function () {
+  var now   = Date.now(),
+      delta = now - then;
+
+  update(delta / 1000);
+  render();
+
+  then = now;
+
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame
+w.requestAnimationFrame = w.requestAnimationFrame       ||
+                          w.webkitRequestAnimationFrame ||
+                          w.msRequestAnimationFrame     ||
+                          w.mozRequestAnimationFrame;
